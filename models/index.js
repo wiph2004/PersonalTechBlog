0a63359684dbfe0b7f9ff0b1445e820288704e54
wiph2004 <149805523+wiph2004@users.noreply.github.com>
2024-03-11 11:50:07 -0500

    added helpers, updated models index, corrected some html

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
+User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Comment.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Comment };
